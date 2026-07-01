d5ea911c9352c28a8d02e766c35e2931a533183e
snackrunner11 <172506893+Snackrunner11@users.noreply.github.com>
2026-07-01 20:45:23 +0200

    added player logic to move and rotate

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,12 +1,19 @@
 import pygame
-from constants import SCREEN_HEIGHT
-from constants import SCREEN_WIDTH
+from constants import *
 from logger import log_state
+from player import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2))
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,8 +21,19 @@ def main():
         log_state()
         for event in pygame.event.get():
             pass
+
+
         screen.fill("black")
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+        player.draw(screen)
+        
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
+
 
 if __name__ == "__main__":
     main()
